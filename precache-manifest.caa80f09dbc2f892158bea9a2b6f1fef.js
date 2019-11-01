1f6f68660181344d577fb61b9aa27ed3946c7d73
kesh24 <keshnarana4@gmail.com>
2019-11-01 15:21:47 -0400

    Updates

---
+++ precache-manifest.caa80f09dbc2f892158bea9a2b6f1fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbdc984e85f6cd859254fe2cb04813a",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "369bff7aa4ca25dd41a4",
+    "url": "/clickygame/static/css/main.e10bea5d.chunk.css"
+  },
+  {
+    "revision": "49ef6d7d0827c0599ecb",
+    "url": "/clickygame/static/js/2.98690820.chunk.js"
+  },
+  {
+    "revision": "369bff7aa4ca25dd41a4",
+    "url": "/clickygame/static/js/main.167ea7f5.chunk.js"
+  },
+  {
+    "revision": "9e617109b3f2d0aedd5b",
+    "url": "/clickygame/static/js/runtime-main.22c9b625.js"
+  }
+]);
